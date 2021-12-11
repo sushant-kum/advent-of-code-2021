0d1614a9efe3645c0563e0d37ce2d0ffd1716d6b
Sushant Kumar <sushant.kum96@gmail.com>
2021-12-11 22:02:42 +0530

    feat(day9): added solution for day 9

---
+++ src/day09/part1.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+
+interface DepthAround {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+}
+
+const input = JSON.parse(fs.readFileSync('./src/day09/data/input.json', 'utf8'));
+const heightMatrix: number[][] = input.height_matrix;
+const heightMatrixRows: number = heightMatrix.length;
+const heightMatrixCols: number = heightMatrix[0].length;
+
+let riskLevel = 0;
+
+for (let row = 0; row < heightMatrixRows; row++) {
+  for (let col = 0; col < heightMatrixCols; col++) {
+    const depthAround: DepthAround = {
+      top: heightMatrix[row - 1] ? heightMatrix[row - 1][col] : Number.MAX_SAFE_INTEGER,
+      right: heightMatrix[row][col + 1] ?? Number.MAX_SAFE_INTEGER,
+      bottom: heightMatrix[row + 1] ? heightMatrix[row + 1][col] : Number.MAX_SAFE_INTEGER,
+      left: heightMatrix[row][col - 1] ?? Number.MAX_SAFE_INTEGER,
+    };
+
+    if (
+      heightMatrix[row][col] < depthAround.top &&
+      heightMatrix[row][col] < depthAround.right &&
+      heightMatrix[row][col] < depthAround.bottom &&
+      heightMatrix[row][col] < depthAround.left
+    ) {
+      riskLevel += heightMatrix[row][col] + 1;
+    }
+  }
+}
+
+console.log('🚀 ~ file: part1.ts ~ line 16 ~ riskLevel', riskLevel);
---
+++ src/day09/part2.ts
@@ -0,0 +1,67 @@
+import fs from 'fs';
+
+const input = JSON.parse(fs.readFileSync('./src/day09/data/input.json', 'utf8'));
+const heightMatrix: number[][] = input.height_matrix;
+const heightMatrixRows: number = heightMatrix.length;
+const heightMatrixCols: number = heightMatrix[0].length;
+const basinMatrix: boolean[][] = JSON.parse(
+  JSON.stringify(new Array(heightMatrixRows).fill(new Array(heightMatrixCols).fill(undefined)))
+);
+const basinSizes: number[] = [];
+const largestBasinSizes: number[] = new Array(3).fill(0);
+const basinSizeMapping: { [key: string]: number } = {};
+
+function isValidPoint(row: number, col: number): boolean {
+  return row >= 0 && col >= 0 && row < heightMatrixRows && col < heightMatrixCols && basinMatrix[row][col];
+}
+
+function calcBasinSize(originatingRow: number, originatingCol: number, row: number, col: number): void {
+  const directionsRow: number[] = [-1, 0, 1, 0];
+  const directionsCol: number[] = [0, 1, 0, -1];
+
+  basinMatrix[row][col] = false;
+
+  for (let i = 0; i < directionsRow.length; i++) {
+    if (isValidPoint(row + directionsRow[i], col + directionsCol[i])) {
+      basinSizeMapping[originatingRow.toString() + originatingCol.toString()]++;
+      calcBasinSize(originatingRow, originatingCol, row + directionsRow[i], col + directionsCol[i]);
+    }
+  }
+}
+
+for (let row = 0; row < heightMatrixRows; row++) {
+  for (let col = 0; col < heightMatrixCols; col++) {
+    basinMatrix[row][col] = heightMatrix[row][col] === 9 ? false : true;
+  }
+}
+
+for (let row = 0; row < heightMatrixRows; row++) {
+  for (let col = 0; col < heightMatrixCols; col++) {
+    if (basinMatrix[row][col]) {
+      basinSizeMapping[row.toString() + col.toString()] = 1;
+      calcBasinSize(row, col, row, col);
+
+      const basinSize: number = basinSizeMapping[row.toString() + col.toString()];
+
+      if (basinSize) {
+        basinSizes.push(basinSize);
+      }
+    }
+  }
+}
+
+const basinCounts: number = basinSizes.length;
+
+for (let i = 0; i < largestBasinSizes.length && i < basinCounts; i++) {
+  const largestSize: number = Math.max(...basinSizes);
+
+  largestBasinSizes[i] = Math.max(largestSize);
+  basinSizes.splice(basinSizes.indexOf(largestSize), 1);
+}
+
+console.log('🚀 ~ file: index.ts ~ line 18 ~ basinSizes', basinSizes);
+console.log('🚀 ~ file: index.ts ~ line 71 ~ largestBasinSizes', largestBasinSizes);
+console.log(
+  '🚀 ~ file: index.ts ~ line 82 ~ largestBasinSizes.reduce((previousValue, currentValue) => previousValue * currentValue)',
+  largestBasinSizes.reduce((previousValue, currentValue) => previousValue * currentValue)
+);
